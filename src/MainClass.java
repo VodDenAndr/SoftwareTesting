11c1511f6111aae775d84f76a2201097abe2af33
VodDenAndr <45555988+VodDenAndr@users.noreply.github.com>
2018-12-05 17:32:15 +0300

    HomeWork 1,2,3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,19 @@
 public class MainClass {
 
     private int class_number = 20;
+    private String class_string = "Hello,world";
 
     public int getClassNumber()
     {
         int x = class_number;
         return x;
     }
-
+    public int getLocalNumber()
+    {
+        return 14;
+    }
+    public String getClassString()
+    {
+        return class_string;
+    }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,9 +2,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
-@Test
 
+    @Test
     public void testGetLocalNumber ()
+    {
+        int x = this.getLocalNumber();
+        int y = 14;
+        if (x == y) {
+            System.out.println("getLocalNumber равен 14") ;
+        } else
+            Assert.fail("getLocalNumber равен : " + x + " а не 14");
+    }
+    @Test
+    public void testClassNumber()
     {
         int x = this.getClassNumber();
         int y = 45;
@@ -13,4 +23,13 @@ public class MainClassTest extends MainClass {
         } else
         Assert.fail("getClassNumber равен " + x + " а это меньше 45");
     }
+    @Test
+    public void testClassString()
+    {
+        if (this.getClassString().contains("hello")||this.getClassString().contains("Hello") ){
+            System.out.println("ggetClassString cодержит Hello или hello") ;
+        } else
+            Assert.fail("Строка: " + this.getClassString() + " не содержит Hello или hello");
+    }
 }
+
